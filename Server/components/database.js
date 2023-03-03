f069a26ef5ff35d26b1dfd18932a21d6e491938e
Nguyễn Hữu Tứ <95615938+nguyenhuutu270402@users.noreply.github.com>
2023-03-04 02:23:09 +0700

    update 19

--- Server/components/database.js
+++ Server/components/database.js
@@ -2,14 +2,14 @@ var mysql = require('mysql');
 require('dotenv').config();
 
 var connection = mysql.createConnection({
-    // host: process.env.DB_HOST,
-    // user: process.env.DB_USER,
-    // password: process.env.DB_PASSWORD,
-    // database: process.env.DB_DATABASE,
-    host: 'localhost',
-    user: 'root',
-    password: 'root123',
-    database: 'db_comicreading',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    // host: 'localhost',
+    // user: 'root',
+    // password: 'root123',
+    // database: 'db_comicreading',
 
 });
 
