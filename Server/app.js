bb97f0bfbed07c7c3b7d2fb31853cf6303266260
Nguyễn Hữu Tứ <huutu0427@gmail.com>
2024-01-15 14:43:55 +0700

    fix bug cors

--- Server/app.js
+++ Server/app.js
@@ -10,6 +10,7 @@ require('dotenv').config();
 const session = require('express-session');
 const hbs = require('hbs');
 const moment = require('moment');
+const cors = require('cors');
 
 var app = express();
 
@@ -22,6 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use(session({
   secret: process.env.APP_SECRET,
